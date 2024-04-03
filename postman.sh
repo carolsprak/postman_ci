14ffeda02631aee9cd5742489f815083ea3a7f32
Anne Caroline Rocha <carolsprak@gmail.com>
2024-04-03 20:46:44 -0300

    atualizando para push

--- postman.sh
+++ postman.sh
@@ -1,6 +1,6 @@
 #!/bin/bash
 
 source .env
-
+ 
 postman login --with-api-key $API_KEY
 postman collection run 18685023-0053788d-5543-431c-a045-13c044a42385 -i 18685023-acbce8ee-3cfb-4915-9593-d20f193d4fb3
